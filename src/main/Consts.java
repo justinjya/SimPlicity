6dd15113abc5e9715f1f7e6cef03f1062e2c0baa
justinjya <18221016@std.stei.itb.ac.id>
2023-04-24 16:21:29 +0800

    updated mockup ui

--- src/main/Consts.java
+++ src/main/Consts.java
@@ -13,7 +13,7 @@ public abstract class Consts {
     
     // PLAY ARENA SIZE
     public static final int PLAY_ARENA_X_LEFT = 208;
-    public static final int PLAY_ARENA_Y_UP = 48;
+    public static final int PLAY_ARENA_Y_UP = 51;
     public static final int PLAY_ARENA_X_RIGHT = PLAY_ARENA_X_LEFT + (SCALED_TILE * 5);
     public static final int PLAY_ARENA_Y_DOWN = PLAY_ARENA_Y_UP + (SCALED_TILE * 5);
     public static final int OFFSET_Y = 57;
