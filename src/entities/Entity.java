8fe8089a434c3eb68acb597f9fdac67a47ecf8ea
Richard Haris <88923240+r11i@users.noreply.github.com>
2023-04-25 17:13:50 +0700

    Add files via upload

---
+++ src/entities/Entity.java
@@ -0,0 +1,157 @@
+package src.entities;
+
+import src.entities.handlers.*;
+import src.main.Consts;
+
+public abstract class Entity {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int speed = 5;
+    private int direction; // 0 = down, 1 = up, 2 = left, 3 = right
+
+    public Entity(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = Consts.SCALED_TILE * width;
+        this.height = Consts.SCALED_TILE * height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isMoving() {
+        if (KeyHandler.isKeyDown(KeyHandler.KEY_A) || KeyHandler.isKeyDown(KeyHandler.KEY_D) || 
+            KeyHandler.isKeyDown(KeyHandler.KEY_W) || KeyHandler.isKeyDown(KeyHandler.KEY_S)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isMovingDiagonally() {
+        if ((KeyHandler.isKeyDown(KeyHandler.KEY_W) && KeyHandler.isKeyDown(KeyHandler.KEY_A)) ||
+            (KeyHandler.isKeyDown(KeyHandler.KEY_W) && KeyHandler.isKeyDown(KeyHandler.KEY_D)) ||
+            (KeyHandler.isKeyDown(KeyHandler.KEY_S) && KeyHandler.isKeyDown(KeyHandler.KEY_A)) ||
+            (KeyHandler.isKeyDown(KeyHandler.KEY_S) && KeyHandler.isKeyDown(KeyHandler.KEY_D))) {
+            return true;
+        }
+        return false;
+    }
+
+    private void checkCollision(CollisionHandler collisionHandler, int newX, int newY) {
+        if (this instanceof Sim) {
+            if (!collisionHandler.isCollision(newX, newY) && !collisionHandler.isOutsidePlayArea(newX, newY)) {
+                x = newX;
+                y = newY;
+            }
+        }
+        else {
+            if (!collisionHandler.isOutsidePlayArea(newX, newY)) {
+                x = newX;
+                y = newY;
+            }
+        }
+    }
+
+    // FOR THE SIM
+    public void move(CollisionHandler collisionHandler, InteractionHandler interactionHandler) {
+        // Update the entity position when moving
+        int newX = x;
+        int newY = y;
+        int initialSpeed = speed;
+
+        if (isMovingDiagonally()) {
+            speed *= 0.707;
+        }
+
+        if (isMoving()) {
+            if (KeyHandler.isKeyDown(KeyHandler.KEY_A)) {
+                newX -= speed;
+                direction = 2;
+                interactionHandler.moveLeft(newX, newY);
+            }
+            if (KeyHandler.isKeyDown(KeyHandler.KEY_D)) {
+                newX += speed;
+                direction = 3;
+                interactionHandler.moveRight(newX, newY);
+            }
+            if (KeyHandler.isKeyDown(KeyHandler.KEY_W)) {
+                newY -= speed;
+                direction = 1;
+                interactionHandler.moveUp(newX, newY);
+            }
+            if (KeyHandler.isKeyDown(KeyHandler.KEY_S)) {
+                newY += speed;
+                direction = 0;
+                interactionHandler.moveDown(newX, newY);
+            }
+            checkCollision(collisionHandler, newX, newY);
+        }
+        speed = initialSpeed;
+
+        // Keybinds for the entity to interact with the game or window
+        if (KeyHandler.isKeyPressed(KeyHandler.KEY_F)) { // check if the F key has been pressed since the last update
+            System.out.println("press");
+            interactionHandler.interact();
+        }
+    }
+
+    // FOR ADDING OBJECTS
+    public void move(CollisionHandler collisionHandler) {
+        // Update the entity position when moving
+        int newX = x;
+        int newY = y;
+        double speed = this.speed * 12.8;
+        double initialSpeed = speed;
+
+        if(isMovingDiagonally()){
+            speed *= 0.707;
+        }
+ 
+        if (isMoving()) {
+            if (KeyHandler.isKeyPressed(KeyHandler.KEY_A)) {
+                newX -= speed; // Move left by one tile
+
+            }
+            if (KeyHandler.isKeyPressed(KeyHandler.KEY_D)) {
+                newX += speed; // Move right by one tile
+            }
+            if (KeyHandler.isKeyPressed(KeyHandler.KEY_W)) {
+                newY -= speed; // Move up by one tile
+            }
+            if (KeyHandler.isKeyPressed(KeyHandler.KEY_S)) {
+                newY += speed; // Move down by one tile
+            }
+            checkCollision(collisionHandler, newX, newY);
+        }
+        speed = initialSpeed;
+    }
+
+    
+}
